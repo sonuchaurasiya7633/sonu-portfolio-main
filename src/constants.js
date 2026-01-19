995ddd7585101384d74e480218884fb163eb879d
sonuchaurasiya7633 <sonukumar763303@gmail.com>
2026-01-19 19:52:39 +0530

    ok

---
+++ src/constants.js
@@ -0,0 +1,257 @@
+// Skills Section Logo's
+import htmlLogo from "./assets/tech_logo/html.png";
+import cssLogo from "./assets/tech_logo/css.png";
+import sassLogo from "./assets/tech_logo/sass.png";
+import javascriptLogo from "./assets/tech_logo/javascript.png";
+import reactjsLogo from "./assets/tech_logo/reactjs.png";
+import angularLogo from "./assets/tech_logo/angular.png";
+import reduxLogo from "./assets/tech_logo/redux.png";
+import nextjsLogo from "./assets/tech_logo/nextjs.png";
+import tailwindcssLogo from "./assets/tech_logo/tailwindcss.png";
+import gsapLogo from "./assets/tech_logo/gsap.png";
+import materialuiLogo from "./assets/tech_logo/materialui.png";
+import bootstrapLogo from "./assets/tech_logo/bootstrap.png";
+import springbootLogo from "./assets/tech_logo/springboot.png";
+import nodejsLogo from "./assets/tech_logo/nodejs.png";
+import expressjsLogo from "./assets/tech_logo/express.png";
+import mysqlLogo from "./assets/tech_logo/mysql.png";
+import mongodbLogo from "./assets/tech_logo/mongodb.png";
+import firebaseLogo from "./assets/tech_logo/firebase.png";
+import cLogo from "./assets/tech_logo/c.png";
+import cppLogo from "./assets/tech_logo/cpp.png";
+import javaLogo from "./assets/tech_logo/java.png";
+import pythonLogo from "./assets/tech_logo/python.png";
+import typescriptLogo from "./assets/tech_logo/typescript.png";
+import gitLogo from "./assets/tech_logo/git.png";
+import githubLogo from "./assets/tech_logo/github.png";
+import vscodeLogo from "./assets/tech_logo/vscode.png";
+import postmanLogo from "./assets/tech_logo/postman.png";
+import mcLogo from "./assets/tech_logo/mc.png";
+import figmaLogo from "./assets/tech_logo/figma.png";
+import netlifyLogo from "./assets/tech_logo/netlify.png";
+import vercelLogo from "./assets/tech_logo/vercel.png";
+import postgreLogo from "./assets/tech_logo/postgre.png";
+import csharpLogo from "./assets/tech_logo/csharp.png";
+
+// Experience Section Logo's
+import webverseLogo from "./assets/company_logo/webverse_logo.png";
+import agcLogo from "./assets/company_logo/agc_logo.png";
+
+// Education Section Logo's
+import glaLogo from "./assets/education_logo/gla_logo.png";
+import bsaLogo from "./assets/education_logo/bsa_logo.png";
+import vpsLogo from "./assets/education_logo/vps_logo.png";
+
+// Project Section Logo's
+import githubdetLogo from "./assets/work_logo/github_det.png";
+import csprepLogo from "./assets/work_logo/cs_prep.png";
+import movierecLogo from "./assets/work_logo/movie_rec.png";
+import taskremLogo from "./assets/work_logo/task_rem.png";
+import npmLogo from "./assets/work_logo/npm.png";
+import webverLogo from "./assets/work_logo/web_dig.png";
+import cmLogo from "./assets/work_logo/cm.png";
+
+export const SkillsInfo = [
+  {
+    title: "Frontend",
+    skills: [
+      { name: "HTML", logo: htmlLogo },
+      { name: "CSS", logo: cssLogo },
+      { name: "SASS", logo: sassLogo },
+      { name: "JavaScript", logo: javascriptLogo },
+      { name: "React JS", logo: reactjsLogo },
+      { name: "Angular", logo: angularLogo },
+      { name: "Redux", logo: reduxLogo },
+      { name: "Next JS", logo: nextjsLogo },
+      { name: "Tailwind CSS", logo: tailwindcssLogo },
+      { name: "GSAP", logo: gsapLogo },
+      { name: "Material UI", logo: materialuiLogo },
+      { name: "Bootstrap", logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      { name: "Springboot", logo: springbootLogo },
+      { name: "Node JS", logo: nodejsLogo },
+      { name: "Express JS", logo: expressjsLogo },
+      { name: "MySQL", logo: mysqlLogo },
+      { name: "MongoDB", logo: mongodbLogo },
+      { name: "Firebase", logo: firebaseLogo },
+      { name: "PostgreSQL", logo: postgreLogo },
+    ],
+  },
+  {
+    title: "Languages",
+    skills: [
+      { name: "C", logo: cLogo },
+      { name: "C++", logo: cppLogo },
+      { name: "Java", logo: javaLogo },
+      { name: "Python", logo: pythonLogo },
+      { name: "C-Sharp", logo: csharpLogo },
+      { name: "JavaScript", logo: javascriptLogo },
+      { name: "TypeScript", logo: typescriptLogo },
+    ],
+  },
+  {
+    title: "Tools",
+    skills: [
+      { name: "Git", logo: gitLogo },
+      { name: "GitHub", logo: githubLogo },
+      { name: "VS Code", logo: vscodeLogo },
+      { name: "Postman", logo: postmanLogo },
+      { name: "Compass", logo: mcLogo },
+      { name: "Vercel", logo: vercelLogo },
+      { name: "Netlify", logo: netlifyLogo },
+      { name: "Figma", logo: figmaLogo },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: webverseLogo,
+    role: "Web Developer Intern",
+    company: "ApexPlanet software pvt.Ltd",
+    date: "may 21  2025 - 05 july 2025",
+    desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React JS",
+      "TypeScript",
+      "Node JS",
+      "Tailwind CSS",
+      "MongoDb",
+      "Redux",
+      " Next Js",
+    ],
+  },
+  {
+    id: 1,
+    img: agcLogo,
+    role: "web developer Intern",
+    company: "VaultOfCodes.in",
+    date: "June 2025 - july 2025",
+    desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "JavaScript",
+      "Tailwind CSS",
+      "HTML",
+      "CSS",
+      "SQL",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: glaLogo,
+    school: "Jagannath University, jaipur",
+    date: "Sept 2023 - July 2027",
+    grade: "8.5 CGPA",
+    desc: "I am currently pursuing my B.Tech in Computer Science and Engineering from Jagannath University, Jaipur. I have built a strong base in programming, data structures, algorithms, OOP, DBMS, and web development,Along with academics, I actively participate in technical workshops and coding events, which help me enhance my skills and practical knowledge,My time at the university is helping me grow as a developer and preparing me for a career in the IT industry.",
+    degree: "Bachelor of Engineering And Technology - B.Tech",
+  },
+
+  {
+    id: 2,
+    img: bsaLogo,
+    school: "M.L.S.M College, Darbhanga",
+    date: "Sept 2019  - Aug 2021",
+    grade: "81%",
+    desc: "I completed my class 12 education from M.L.S.M College, Darbhanga , under the BSEB board, where I studied Physics, Chemistry, and Mathematics (PCM) with bIOLOGY.",
+    degree: "BSEB(XII) - PCM with BIOLOGY",
+  },
+  {
+    id: 3,
+    img: vpsLogo,
+    school: "U.M.T School Sanahpur, Darbhanga",
+    date: "Apr 2018 - March 2019",
+    grade: "76.4%",
+    desc: "I completed my class 10 education from U.M.T School Sanahpur,  Darbhanga, under the BSEB board, where I studied All Subject ",
+    degree: "BSEB(X), All Subject ",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Success Point School",
+    description:
+      "A modern and interactive web application built with React.js for Success Point School. It showcases school information, courses, faculty details, events, and contact forms. Designed with an intuitive UI and responsive design, the app helps students, parents, and staff easily access important updates and resources about the school.",
+
+    image: csprepLogo,
+    tags: ["HTML", "CSS", "JavaScript", "React JS", "API"],
+    github:
+      "https://github.com/sonuchaurasiya7633/school",
+    webapp: "https://success-point-school-sandy.vercel.app/",
+  },
+  {
+    id: 1,
+    title: "CRUD App",
+    description:
+      "A full-stack application that allows managing student records with ease. Users can create, view, update, and delete student details through an intuitive interface. Built with modern technologies, this app demonstrates the core CRUD functionalities essential for database-driven applications.",
+
+    image: movierecLogo,
+    tags: ["React JS", "HTML", "CSS", "JavaScript"],
+    github: "https://github.com/sonuchaurasiya7633/crud",
+    webapp: "https://crud-app-alpha-liard.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "DeepSeek Chatbot",
+    description:
+      "A simple React-based frontend clone of the DeepSeek Chatbot, designed to mimic its interface and provide basic chatbot functionality. This project focuses on UI replication and user interaction without advanced backend features.",
+
+    image: githubdetLogo,
+    tags: ["React JS", "HTML", "CSS", "JavaScript"],
+    github: "https://github.com/sonuchaurasiya7633/deepseek",
+    webapp: "https://deepseek-dun.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "ShopCode Ecommerce",
+    description:
+      "A full-featured React.js and Node.js based e-commerce application that includes product browsing, cart management, and secure payments integration using Razorpay. Designed for a smooth user experience and easy customization.",
+    image: cmLogo,
+    tags: ["React JS", "Node.js", "NPM", "Validation", "Payment Integration"],
+    github: "https://github.com/sonuchaurasiya7633/shopcode",
+    webapp: "https://shopcode-gamma.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Food Delivery App",
+    description:
+      "A modern and user-friendly web application for browsing restaurants, exploring menus, and placing food orders online. Built with React.js and integrated with essential features like real-time order tracking and secure checkout for a seamless food delivery experience.",
+    image: webverLogo,
+    tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
+    github: "https://github.com/sonuchaurasiya7633/E-Commerce-Web",
+    webapp: "https://sonu-food-delevery.netlify.app/",
+  },
+  {
+    id: 5,
+    title: "URL Shortener",
+    description:
+      "A simple and efficient web application that allows users to shorten long URLs into concise, shareable links. Built with HTML, CSS, and JavaScript, it provides an easy-to-use interface to generate, manage, and redirect shortened URLs.",
+    image: taskremLogo,
+    tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
+    github: "https://github.com/sonuchaurasiya7633/URL_SHortner",
+    webapp: "https://url-shortner-n0b3.onrender.com",
+  },
+  {
+    id: 6,
+    title: "Image Uploader",
+    description:
+      "A full-stack image uploader app where users can upload, store, and manage images securely. Built with React.js, Node.js, and Cloudinary, it offers real-time preview and responsive design.",
+    image: npmLogo,
+    tags: ["React JS", "Node.js", "MongoDB", "Express"],
+    github: "https://github.com/sonuchaurasiya7633/Image_Uploader_sonu",
+    webapp: "https://image-uploader-sonu.onrender.com",
+  },
+];
